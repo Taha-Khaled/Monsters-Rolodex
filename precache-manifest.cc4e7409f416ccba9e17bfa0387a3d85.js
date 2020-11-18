f0b12d8b6b17aec279b413b63ba4da404300c442
taha apps <appsbytaha@gmail.com>
2020-11-18 08:43:03 +0200

    Updates

---
+++ precache-manifest.cc4e7409f416ccba9e17bfa0387a3d85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d5642b0f853ac07fc20f2b338ddf5f",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "2b581707bebe183f8abe",
+    "url": "/Monsters-Rolodex/static/css/main.70d0490d.chunk.css"
+  },
+  {
+    "revision": "88fbeb16f8c4e6e689a6",
+    "url": "/Monsters-Rolodex/static/js/2.6333a692.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Monsters-Rolodex/static/js/2.6333a692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b581707bebe183f8abe",
+    "url": "/Monsters-Rolodex/static/js/main.b5075126.chunk.js"
+  },
+  {
+    "revision": "0c3b088eb4773481776b",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.27805cdf.js"
+  }
+]);
